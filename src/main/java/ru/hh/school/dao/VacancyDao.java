e595e9cb79dc63970193ef36654d541f1a3c2284
Alexander <aadisalimov@sberbank.ru>
2023-12-15 21:20:02 +0300

    homework

--- src/main/java/ru/hh/school/dao/VacancyDao.java
+++ src/main/java/ru/hh/school/dao/VacancyDao.java
@@ -12,10 +12,10 @@ public class VacancyDao extends GenericDao{
   public StatisticsDto getSalaryStatistics(Area area){
     // ToDo дополните запрос, чтобы возвращался ru.hh.school.employers.StatisticsDto
     // https://vladmihalcea.com/the-best-way-to-map-a-projection-query-to-a-dto-with-jpa-and-hibernate/
-    return getSession().createQuery(
-        "SELECT count(v.id), min(v.compensationFrom), max(v.compensationTo) " +
-            "FROM Vacancy v WHERE v.area = :area", StatisticsDto.class)
-        .setParameter("area", area)
-        .getSingleResult();
+    return getSession().createQuery("SELECT NEW ru.hh.school.employers.StatisticsDto(count(v.id), " +
+                    "min(v.compensationFrom), max(v.compensationTo)) " +
+                    "FROM Vacancy v WHERE v.area = :area", StatisticsDto.class)
+            .setParameter("area", area)
+            .getSingleResult();
   }
 }
--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -1,9 +1,14 @@
 package ru.hh.school.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 //TODO: оформите entity
+@Entity
 public class Resume {
   // TODO: сделать так, чтобы id брался из sequence-а
   // таким образом, мы сможем отправлять в бд запросы батчами.
@@ -15,12 +20,14 @@ public class Resume {
   // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
 
   @Id
-  @GeneratedValue(/* здесь место для вашего кода */)
+  @GeneratedValue(generator = "resume_id_seq", strategy = GenerationType.SEQUENCE)
+  @SequenceGenerator(name = "resume_id_seq", sequenceName = "resume_id_seq", allocationSize = 10)
   private Integer id;
 
+  @Column
   private String description;
 
-  Resume() {}
+  protected Resume() {}
 
   public Resume(String description) {
     this.description = description;
